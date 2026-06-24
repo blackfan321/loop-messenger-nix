6323f1864a57aedb1a8bbf84bf86466bc15aac12
Ivan Batrakov <blackfan321@gmail.com>
2026-06-24 09:18:56 +0300

    fix WMClass

--- desktop-helper.nix
+++ desktop-helper.nix
@@ -2,6 +2,7 @@
 ''
   install -m 444 -D ${appimageContents}/${desktopFile} $out/share/applications/${pname}.desktop
   substituteInPlace $out/share/applications/${pname}.desktop \
-    --replace 'Exec=AppRun' 'Exec=${pname}'
+    --replace 'Exec=AppRun' 'Exec=${pname}' \
+    --replace 'StartupWMClass=LOOP' 'StartupWMClass=Loop'
   cp -r ${appimageContents}/usr/share/icons $out/share
 ''
